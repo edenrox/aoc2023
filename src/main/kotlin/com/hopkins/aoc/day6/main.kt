ea94321081ac422e164d0d952cd30f38a0b1ca17
Ian Hopkins <ian@lucidhelix.com>
2023-12-07 09:37:17 -0500

    Adding some comments

--- src/main/kotlin/com/hopkins/aoc/day6/main.kt
+++ src/main/kotlin/com/hopkins/aoc/day6/main.kt
@@ -25,18 +25,29 @@ fun main() {
     }
     val result: Int =
         ways.fold(1) { acc, item -> acc * item }
-    print("Result: $result")
+    print("Result: $result") // 588588
 }
 
+/**
+ * Returns the number of ways in which the boat can travel the distance.
+ *
+ * Start with holding the button for 1 second, then 2 seconds, etc.  then calculate
+ * the distance travelled in the time remaining.
+ */
 fun findWays(totalTime: Int, distance: Int): Int {
     return IntRange(1, totalTime - 1).count { calculateDistanceTravelled(it, totalTime) > distance }
 }
 
+/** Calculate the distance traveled given the time held and total time. */
 fun calculateDistanceTravelled(timeHeld: Int, totalTime: Int): Int {
+    // Speed = 1 mm/s for each second we hold the button
     val speed = timeHeld
+
+    // Distance (mm) = Speed (mm/s) * Time remaining (s)
     return speed * (totalTime - timeHeld)
 }
 
+/** Returns list of integers after the ":" separated by 1 or more spaces. */
 fun readValues(line: String): List<Int> {
     val (_, values) = line.split(":")
     return values.trim().split(" ").filterNot { it.trim().isBlank() }.map { it.toInt() }
