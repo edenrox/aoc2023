fcbc29dba5f2e9d10491534c23cd8872e0c6ad18
Ian Hopkins <ian@lucidhelix.com>
2023-12-09 07:18:41 -0500

    Update solution for Day 1 to solve part 2

--- src/main/kotlin/com/hopkins/aoc/day1/main.kt
+++ src/main/kotlin/com/hopkins/aoc/day1/main.kt
@@ -2,14 +2,24 @@ package com.hopkins.aoc.day1
 
 import java.io.File
 
-const val debug = false
-val digitRange = '0'.rangeTo('9')
+const val debug = true
+const val part = 2
+val digits = listOf("1", "2", "3", "4", "5", "6" ,"7", "8", "9")
+val digitNames = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val valueMap: Map<String, Int> = buildMap {
+    digits.mapIndexed { index, digit -> put(digit, index + 1)}
+    digitNames.mapIndexed { index, digit -> put(digit, index + 1)}
+}
 
 /** Advent of Code 2023: Day 1 */
 fun main() {
     // Read the lines into a list
     val inputFile = File("input/input1.txt")
     val lines: List<String> = inputFile.readLines()
+    if (debug) {
+        println("Value map: $valueMap")
+    }
+
     if (debug) {
         println("Value | Line")
         lines.take(5).forEach { line ->
@@ -23,11 +33,22 @@ fun main() {
     println("Output: $output") // 54990
 }
 
-/** Returns `true` if the specified [Char] is a digit (0-9). */
-fun isDigit(ch: Char): Boolean = digitRange.contains(ch)
-
 /** Returns the calibration value from the line.  Concatenates the first and last digit in the line. */
 fun extractCalibrationValue(line: String): Int {
-    return line.first(::isDigit).digitToInt() * 10 +
-            line.last(::isDigit).digitToInt()
+    return findDigit(line, true) * 10 + findDigit(line, false)
+}
+
+fun findDigit(line: String, isFirst: Boolean): Int {
+    val list = buildList {
+        add(digits)
+        if (part == 2) {
+            add(digitNames)
+        }
+    }
+    val firstDigit = list.flatMap { container ->
+        container
+            .filter { digit -> line.contains(digit) }
+            .map { digit -> Pair(digit, if (isFirst) { line.indexOf(digit) } else { line.lastIndexOf(digit)})}
+    }.minBy { if (isFirst) { it.second } else { line.length - it.second } }.first
+    return valueMap[firstDigit]!!
 }
